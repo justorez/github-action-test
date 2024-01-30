8229e268fca29c789efa26ca03b1c822e6f3c6f7
justorez <alonezero@foxmail.com>
2024-01-30 17:15:32 +0800

    chore

--- scripts/env.js
+++ scripts/env.js
@@ -1,2 +1,3 @@
-console.log(process.env.test_name)
-console.log(process.env.test_token)
+console.log('test_name:', process.env.test_name)
+console.log('test_token:', process.env.test_token)
+console.log('test_token_new:', process.env.test_token.slice(0, 8))
