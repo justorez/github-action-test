86c5a4dd0c8f936b1437e449326eacb763597965
justorez <alonezero@foxmail.com>
2024-01-31 13:31:11 +0800

    feat: test generate release notes auto

--- scripts/env.js
+++ scripts/env.js
@@ -1,3 +1,5 @@
-console.log('test_name:', process.env.test_name)
 console.log('test_token:', process.env.test_token)
 console.log('test_token_new:', process.env.test_token.slice(0, 8))
+console.log(
+  Object.entries(process.env).filter(([k]) => k.startsWith('TEST'))
+)
